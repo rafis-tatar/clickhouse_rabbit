dc4e1ce0b09fa362e009176d47fbf8f86c05ded8
Гатауллин Рафис <GataullinRA@tatneft.ru>
2021-11-24 09:37:05 +0300

    добавил конфиги для Rabbit, создание пользователей

--- clickhouse/createTables.sql
+++ clickhouse/createTables.sql
@@ -1,33 +1,33 @@
-CREATE TABLE IF NOT EXISTS cars (
-  device_id String,
-  datetime DateTime,
-  latitude Float32,
-  longitude Float32,
-  altitude Float32,
-  speed Float32,
-  battery_voltage Nullable(Float32),
-  cabin_temperature Float32,
-  fuel_level Nullable(Float32)
+CREATE TABLE IF NOT EXISTS audit_log (
+  EventType String,
+  EventName String,
+  ApplicationName String,
+  ModuleName String,
+  InstanceName String,
+  UserName String,
+  Guid UUID,
+  EventTime DateTime,
+  Value String
 ) ENGINE = RabbitMQ
 SETTINGS
   rabbitmq_host_port = 'rabbit:5672',
   rabbitmq_skip_broken_messages = 1,
-  rabbitmq_routing_key_list = 'cars',
+  rabbitmq_routing_key_list = 'audit_log',
   rabbitmq_exchange_name = 'exchange',
   rabbitmq_format = 'JSONEachRow';
 
-CREATE TABLE IF NOT EXISTS cars_data_source (
-    device_id String,
-    datetime DateTime,
-    latitude Float32,
-    longitude Float32,
-    altitude Float32,
-    speed Float32,
-    battery_voltage Nullable(Float32),
-    cabin_temperature Float32,
-    fuel_level Nullable(Float32)
+CREATE TABLE IF NOT EXISTS audit_log_data_source (
+    EventType String,
+    EventName String,
+    ApplicationName String,
+    ModuleName String,
+    InstanceName String,
+    UserName String,    
+    Guid UUID,
+    EventTime DateTime,
+    Value String
 ) ENGINE MergeTree()
   ORDER BY device_id;
 
-CREATE MATERIALIZED VIEW cars_view TO cars_data_source
-  AS SELECT * FROM cars;
+CREATE MATERIALIZED VIEW audit_log_view TO audit_log_data_source
+  AS SELECT * FROM audit_log;
